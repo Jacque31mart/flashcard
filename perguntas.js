dd5c7164a6ae95aa280b46bac81eefa802f8a6ef
Jacque31mart <jacque31moranguinho@icloud.com>
2024-11-26 09:48:05 -0300

    projeto concluído.

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Um conjunto de instruções para resolver um problema'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Itália?',
+    'A capital da Itália é Roma'
+)
+
+criaCartao(
+    'Quimíca',
+    'Qual a fórmula quimica do dióxido de carbono?',
+    'CO2'
+)
+
+criaCartao(
+    'Astronomia',
+    'Quantos planetas existem no sistema solar e quais seus nomes?',
+    'Existem oito planetas no sistema solar. Mercúrio,Vênus,Terra,Marte,Júpiter,Saturno,Urano e Netuno'
+)
